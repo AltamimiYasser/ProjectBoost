5dfe6b2c0622cf8e0117889231253dc17846e39a
yasser tamimi <altamimiy89@gmail.com>
2022-07-26 05:23:16 +0300

    auto movment of the object between point A to point B

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -5,6 +5,7 @@ public class Oscillator : MonoBehaviour
     Vector3 startingPos;
     [SerializeField] Vector3 movementVector;
     [SerializeField][Range(0, 1)] float movementFactor;
+    [SerializeField] float period = 2f;
 
     void Start()
     {
@@ -14,6 +15,11 @@ public class Oscillator : MonoBehaviour
 
     void Update()
     {
+        float cycles = Time.time / period;
+        const float tau = Mathf.PI * 2;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+        movementFactor = (rawSinWave + 1f) / 2f;
+
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPos - offset;
     }
