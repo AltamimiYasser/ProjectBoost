0d68b9405495cae557c8928804c595881144089a
yasser tamimi <altamimiy89@gmail.com>
2022-07-26 04:50:34 +0300

    moving obstcale from one point to another back and forth

---
+++ Assets/Oscillator.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    Vector3 startingPos;
+    [SerializeField] Vector3 movementVector;
+    [SerializeField][Range(0, 1)] float movementFactor;
+
+    void Start()
+    {
+        startingPos = transform.position;
+    }
+
+
+    void Update()
+    {
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos - offset;
+    }
+}
